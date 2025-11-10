6aaf892b753c4ac2497a3cce58350d5ec8afe648
Quanlong Guan <131424177+chanllon@users.noreply.github.com>
2025-11-10 14:07:19 +0800

    Add files via upload

---
+++ EduSim/Envs/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from .KSS import KSSEnv
+from .KES_ASSIST09 import KESASSISTEnv
+from .KES_Junyi import KESEnv
+from .KES_MOOCCube import KESMOOCCubeEnv
---
+++ EduSim/__init__.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+from gym.envs.registration import register
+from .Envs import *
+from .SimOS import train_eval, MetaAgent
+from .spaces import *
+
+register(
+    id='KSS-v2',
+    entry_point='EduSim.Envs:KSSEnv',
+)
+
+
+register(
+    id='KES-v1',
+    entry_point='EduSim.Envs:KESASSISTEnv',
+)
+
+
+
+
+
+register(
+    id='MBS-EFC-v0',
+    entry_point='EduSim.Envs:EFCEnv',
+)
+
+register(
+    id='MBS-HLR-v0',
+    entry_point='EduSim.Envs:HLREnv',
+)
+
+register(
+    id='MBS-GPL-v0',
+    entry_point='EduSim.Envs:GPLEnv',
+)
+
+register(
+    id='TMS-v1',
+    entry_point='EduSim.Envs:TMSEnv',
+)
